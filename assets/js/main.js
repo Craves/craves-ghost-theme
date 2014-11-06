d03055bbe6f938435e6614ec439cde706df43f6d
Scott Cormier <scottcormier@gmail.com>
2014-11-06 13:02:58 -0700

    Updated layout for mobile views and added responsive menu

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$( "#menuBtn" ).click(function() {
+  $("#menuContainer").toggleClass("in-view");
+  $("#menuContainer").toggleClass("unstyled");
+  var btnCopy = $(this).text();
+  $(this).text( btnCopy == "Menu" ? "Close" : "Menu");
+});
